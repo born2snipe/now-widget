829cb872a1f6633d27855bfce4eade07b0c7e6b4
born2snipe <born2snipe@gmail.com>
2010-04-24 09:14:02 -0500

    initial

---
+++ src/main/java/b2s/fitnesse/widget/App.java
@@ -0,0 +1,13 @@
+package b2s.fitnesse.widget;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
